52fc3c9987b422c78080ceb19419509ab7440425
pc2752 <pc2752@gmail.com>
2019-04-05 14:25:09 +0200

    Everything is working here, will shift toh GMM now

--- prep_data_v1.py
+++ prep_data_v1.py
@@ -75,74 +75,105 @@ def prep_deepsalience():
 
         song_name = [x.split('_')[0] for x in singers][0]
 
+
+
         count = 0
 
         for combo in combos:
             combo_str = str(combo[0]) + str(combo[1]) + str(combo[2]) + str(combo[3])
 
+            # combo = [3,2,3,4]
+
             if not os.path.isfile(config.feats_dir+song_name+'_'+combo_str+'.hdf5'):
                 if combo[0]!=0:
                     audio_sop, fs = librosa.core.load(os.path.join(song_dir,song_name+'_soprano_'+str(combo[0])+'.wav'), sr = config.fs)
-                    f0 = pitch.extract_f0_sac(audio_sop, fs, config.hoptime).reshape(-1,1)
-                    atb_sop = process_f0(f0, f_bins, n_freqs)
+                    f0_sop = pitch.extract_f0_sac(audio_sop, fs, config.hoptime).reshape(-1,1)
+                    f0 = np.zeros((len(f0_sop),4))
+                    f0[:,0] = f0_sop[:,0]
+                    atb_sop = process_f0(f0_sop, f_bins, n_freqs)
                     audio = audio_sop
                     atb = atb_sop
                 if combo[1]!=0:
                     audio_alt, fs = librosa.core.load(os.path.join(song_dir,song_name + '_alto_' + str(combo[1]) + '.wav'), sr=config.fs)
-                    f0 = pitch.extract_f0_sac(audio_alt, fs, config.hoptime).reshape(-1,1)
-                    atb_alt = process_f0(f0, f_bins, n_freqs)
+                    f0_alt = pitch.extract_f0_sac(audio_alt, fs, config.hoptime).reshape(-1,1)
+                    atb_alt = process_f0(f0_alt, f_bins, n_freqs)
                     if combo[0] != 0:
                         audio = audio[:audio_alt.shape[0]] + audio_alt[:audio.shape[0]]
                         atb = atb[:atb_alt.shape[0]] + atb_alt[:atb.shape[0]]
                     else:
+                        f0 = np.zeros((len(f0_alt),4))
                         audio = audio_alt
                         atb = atb_alt
+                    f0[:f0_alt.shape[0],1] =f0_alt[:f0.shape[0],0]
                 if combo[2]!=0:
                     audio_bas, fs = librosa.core.load(os.path.join(song_dir, song_name + '_bass_' + str(combo[2]) + '.wav'), sr=config.fs)
-                    f0 = pitch.extract_f0_sac(audio_bas, fs, config.hoptime).reshape(-1,1)
-                    atb_bas = process_f0(f0, f_bins, n_freqs)
+                    f0_bas = pitch.extract_f0_sac(audio_bas, fs, config.hoptime).reshape(-1,1)
+                    atb_bas = process_f0(f0_bas, f_bins, n_freqs)
 
                     if combo[0] == 0 and combo[1] ==0 :
                         audio = audio_bas
                         atb = atb_bas
+                        f0 = np.zeros((len(f0_bas),4))
                     else:
                         audio = audio[:audio_bas.shape[0]] + audio_bas[:audio.shape[0]]
                         atb = atb[:atb_bas.shape[0]] + atb_bas[:atb.shape[0]]
+                    f0[:f0_bas.shape[0],2] =f0_bas[:f0.shape[0],0]
+
                 if combo[3]!=0:
                     audio_ten, fs = librosa.core.load(os.path.join(song_dir, song_name + '_tenor_' + str(combo[3]) + '.wav'), sr=config.fs)
-                    f0 = pitch.extract_f0_sac(audio_ten, fs, config.hoptime).reshape(-1,1)
-                    atb_ten = process_f0(f0, f_bins, n_freqs)
+                    f0_ten = pitch.extract_f0_sac(audio_ten, fs, config.hoptime).reshape(-1,1)
+                    atb_ten = process_f0(f0_ten, f_bins, n_freqs)
                     if combo[0]== 0 and combo[1]== 0 and combo[2] == 0:
                         audio = audio_ten
                         atb = atb_ten
+                        f0 = np.zeros((len(f0_ten),4))
                     else:
                         audio = audio[:audio_ten.shape[0]] + audio_ten[:audio.shape[0]]
                         atb = atb[:atb_ten.shape[0]] + atb_ten[:atb.shape[0]]
+                    f0[:f0_ten.shape[0],3] =f0_ten[:f0.shape[0],0]
+
                 num_sources = 4 - combo.count(0)
                 audio = audio/num_sources
 
                 voc_stft, voc_cqt, voc_hcqt = sig_process.get_feats(audio)
 
-                assert voc_stft.shape[0] == atb.shape[0]
+                min_len = min(len(voc_hcqt), len(atb), len(f0))
+
+                voc_hcqt = voc_hcqt[:min_len]
+
+                atb = atb[:min_len]
+
+                f0 = f0[:min_len]
+
+                zeros = 1 - (f0==0).astype(int)
 
 
 
 
-                hdf5_file = h5py.File(config.feats_dir+song_name+'_'+combo_str+'.hdf5', mode='w')
+                hdf5_file = h5py.File(config.feats_dir+song_name+'_'+combo_str+'.hdf5', mode='a')
 
-                hdf5_file.create_dataset("voc_stft", voc_stft.shape, np.complex64)
+                # hdf5_file.create_dataset("voc_stft", voc_stft.shape, np.complex64)
 
-                hdf5_file.create_dataset("voc_cqt", voc_cqt.shape, np.complex64)
+                # hdf5_file.create_dataset("voc_cqt", voc_cqt.shape, np.complex64)
+
+
 
                 hdf5_file.create_dataset("voc_hcqt", voc_hcqt.shape, np.float64)
 
+                
                 hdf5_file.create_dataset("atb", atb.shape, np.float64)
 
+                hdf5_file.create_dataset("f0", f0.shape, np.float64)
+
+                hdf5_file.create_dataset("zeros", zeros.shape, np.float64)
+
+                
 
 
-                hdf5_file["voc_stft"][:,:] = voc_stft
 
-                hdf5_file["voc_cqt"][:,:] = voc_cqt
+                hdf5_file["f0"][:,:] = f0
+
+                hdf5_file["zeros"][:,:] = zeros
 
                 hdf5_file["voc_hcqt"][:,:] = voc_hcqt
 
@@ -150,6 +181,10 @@ def prep_deepsalience():
 
 
                 hdf5_file.close()
+
+                # import pdb;pdb.set_trace()
+
+                # import pdb;pdb.set_trace()
 
 
             count+=1
@@ -213,5 +248,5 @@ def prep_voc_sep():
 
 
 if __name__ == '__main__':
-    prep_voc_sep()
-    # prep_deepsalience()
+    # prep_voc_sep()
+    prep_deepsalience()
